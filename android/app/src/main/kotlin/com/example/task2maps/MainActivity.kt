3a3f369d56fb2a482924a63fbc8ea2e93ec6c7fd
apple <farooqafridi958@gmail.com>
2023-09-16 16:36:00 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/task2maps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.task2maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyCmCwk1GwwxBWJgBPKL2j9gTY91uYqNtYk';
+}
